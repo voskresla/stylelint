fe1932a5211178e14dcf6bdc91372a245a4b941d
jeddy3 <jeddy3@users.noreply.github.com>
2020-04-28 13:23:30 +0100

    Add temp test script

---
+++ test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const stylelint = require('./lib/index.js');
+
+stylelint
+	.lint({
+		config: { rules: { 'color-hex-length': 'long' } },
+		code: `
+			a {
+				color: #fff}
+		`,
+		//syntax: 'sass',
+		//fix: true,
+		//formatter: 'verbose',
+	})
+	.then((data) => {
+		/* eslint-disable-next-line no-console */
+		console.log(data);
+	})
+	.catch((err) => {
+		/* eslint-disable-next-line no-console */
+		console.error(err.stack);
+	});
